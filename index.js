a1fbdcbf2353001ed3d23fc399a8a98b00427a06
magicdirac <magicdirac@users.noreply.github.com>
2018-07-19 17:58:47 +0800

    try out for handling get request.

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 app.get('/api', function(req, res){
   res.send({ name: 'Yoshi' });
 });
-
+app.get("/", function(req, res) {
+    console.log("Get Request.");
+    res.end();
+})
 // listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requests');
